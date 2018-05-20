1090e9cf680bbfaa1b49bf96268a63a6ae8d08c5
xupengfei <1830166767@qq.com>
2018-05-20 17:50:19 +0800

    分层

--- src/main/java/com/flyer/fbs/StartApp.java
+++ src/main/java/com/flyer/fbs/StartApp.java
@@ -2,11 +2,13 @@ package com.flyer.fbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FbsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FbsApplication.class, args);
 	}
+
 }
