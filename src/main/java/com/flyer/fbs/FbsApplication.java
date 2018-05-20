3391f5c47f798a9224b04c52784185471cf032c9
xupengfei <1830166767@qq.com>
2018-05-20 17:26:30 +0800

    Initial commit

---
+++ src/main/java/com/flyer/fbs/FbsApplication.java
@@ -0,0 +1,12 @@
+package com.flyer.fbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbsApplication.class, args);
+	}
+}
