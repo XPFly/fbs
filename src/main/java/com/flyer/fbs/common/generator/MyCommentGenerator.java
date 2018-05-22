c0f7385f5fb4a483afcc03d557f4b34e0888ac0e
xupengfei <1830166767@qq.com>
2018-05-22 11:27:59 +0800

    添加代码生成器

---
+++ src/main/java/com/flyer/fbs/common/generator/MyCommentGenerator.java
@@ -0,0 +1,253 @@
+package com.flyer.fbs.common.generator;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.config.PropertyRegistry;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Set;
+
+import static org.mybatis.generator.internal.util.StringUtility.isTrue;
+
+/**
+ * @description: 自定义注释内容
+ * @author: xu_pf@suixingpay.com
+ * @date: 2018/5/22
+ */
+public class MyCommentGenerator implements CommentGenerator {
+
+    private Properties properties;
+    private Properties systemPro;
+    private boolean suppressDate;
+    private boolean suppressAllComments;
+    private String currentDateStr;
+
+    public MyCommentGenerator() {
+        super();
+        properties = new Properties();
+        systemPro = System.getProperties();
+        suppressDate = false;
+        suppressAllComments = false;
+        currentDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
+    }
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        this.properties.putAll(properties);
+
+        suppressDate = isTrue(properties.getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_DATE));
+
+        suppressAllComments = isTrue(properties.getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_ALL_COMMENTS));
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        if (suppressAllComments) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        field.addJavaDocLine("/**");
+        sb.append(" * ");
+        sb.append(introspectedColumn.getRemarks());
+        field.addJavaDocLine(sb.toString().replace("\n", " "));
+        field.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+        if (suppressAllComments) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        field.addJavaDocLine("/**");
+        sb.append(" * ");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        field.addJavaDocLine(sb.toString().replace("\n", " "));
+        field.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+        if (suppressAllComments) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        innerClass.addJavaDocLine("/**");
+        sb.append(" * ");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append(" ");
+        sb.append(getDateString());
+        innerClass.addJavaDocLine(sb.toString().replace("\n", " "));
+        innerClass.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+        if (suppressAllComments) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        innerClass.addJavaDocLine("/**");
+        sb.append(" * ");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        innerClass.addJavaDocLine(sb.toString().replace("\n", " "));
+        sb.setLength(0);
+        sb.append(" * @author ");
+        sb.append(systemPro.getProperty("user.name"));
+        sb.append(" ");
+        sb.append(currentDateStr);
+        innerClass.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+        if (suppressAllComments) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        innerEnum.addJavaDocLine("/**");
+        sb.append(" * ");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        innerEnum.addJavaDocLine(sb.toString().replace("\n", " "));
+        innerEnum.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        if (suppressAllComments) {
+            return;
+        }
+//        method.addJavaDocLine("/**");
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(" * ");
+//        sb.append(introspectedColumn.getRemarks());
+//        method.addJavaDocLine(sb.toString().replace("\n", " "));
+//        sb.setLength(0);
+//        sb.append(" * @return ");
+//        sb.append(introspectedColumn.getActualColumnName());
+//        sb.append(" ");
+//        sb.append(introspectedColumn.getRemarks());
+//        method.addJavaDocLine(sb.toString().replace("\n", " "));
+//        method.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        if (suppressAllComments) {
+            return;
+        }
+//        method.addJavaDocLine("/**");
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(" * ");
+//        sb.append(introspectedColumn.getRemarks());
+//        method.addJavaDocLine(sb.toString().replace("\n", " "));
+//        Parameter parm = method.getParameters().get(0);
+//        sb.setLength(0);
+//        sb.append(" * @param ");
+//        sb.append(parm.getName());
+//        sb.append(" ");
+//        sb.append(introspectedColumn.getRemarks());
+//        method.addJavaDocLine(sb.toString().replace("\n", " "));
+//        method.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+        if (suppressAllComments) {
+            return;
+        }
+        //method.addJavaDocLine("/**");
+        //addJavadocTag(method, false);
+        //method.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addRootComment(XmlElement rootElement) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    /**
+     * This method adds the custom javadoc tag for. You may do nothing if you do
+     * not wish to include the Javadoc tag - however, if you do not include the
+     * Javadoc tag then the Java merge capability of the eclipse plugin will
+     * break.
+     *
+     * @param javaElement
+     *            the java element
+     */
+    protected void addJavadocTag(JavaElement javaElement, boolean markAsDoNotDelete) {
+        javaElement.addJavaDocLine(" *");
+        StringBuilder sb = new StringBuilder();
+        sb.append(" * ");
+        sb.append("flyer");
+        if (markAsDoNotDelete) {
+            sb.append(" do_not_delete_during_merge");
+        }
+        String s = getDateString();
+        if (s != null) {
+            sb.append(' ');
+            sb.append(s);
+        }
+        javaElement.addJavaDocLine(sb.toString());
+    }
+
+    /**
+     * This method returns a formated date string to include in the Javadoc tag
+     * and XML comments. You may return null if you do not want the date in
+     * these documentation elements.
+     *
+     * @return a string representing the current timestamp, or null
+     */
+    protected String getDateString() {
+        String result = null;
+        if (!suppressDate) {
+            result = currentDateStr;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/flyer/fbs/common/generator/StartUpGenerator.java
@@ -0,0 +1,12 @@
+package com.flyer.fbs.common.generator;
+
+/**
+ * @description: 代码生成器|启动类
+ * @author: xu_pf@suixingpay.com
+ * @date: 2018/5/22
+ */
+public class StartUpGenerator {
+    public static void main(String[] args) {
+
+    }
+}
