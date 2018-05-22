4c3b841ecf0cf1d68c4c333de8e7af29a80b2633
xupengfei <1830166767@qq.com>
2018-05-22 15:33:23 +0800

    代码生成器|启动类

--- src/main/java/com/flyer/fbs/common/generator/StartUpGenerator.java
+++ src/main/java/com/flyer/fbs/common/generator/StartUpGenerator.java
@@ -1,5 +1,14 @@
 package com.flyer.fbs.common.generator;
 
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @description: 代码生成器|启动类
  * @author: xu_pf@suixingpay.com
@@ -7,6 +16,18 @@ package com.flyer.fbs.common.generator;
  */
 public class StartUpGenerator {
     public static void main(String[] args) {
-
+        try {
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
